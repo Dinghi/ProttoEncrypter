edfea21b1d5e69aacb8d6ba3a25b2897ffced212
Francesco Ostento <francesco.ostento@gmail.com>
2020-08-12 13:14:04 +0200

    First Form created with crypter and decrypter

--- ProttoEncrypter/Form1.Designer.cs
+++ ProttoEncrypter/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button DecryptaButton;
+            this.NormalText = new System.Windows.Forms.RichTextBox();
+            this.EncrypterText = new System.Windows.Forms.RichTextBox();
+            this.CryptaButton = new System.Windows.Forms.Button();
+            this.PassphraseText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            DecryptaButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NormalText
+            // 
+            this.NormalText.Location = new System.Drawing.Point(12, 117);
+            this.NormalText.Name = "NormalText";
+            this.NormalText.Size = new System.Drawing.Size(401, 87);
+            this.NormalText.TabIndex = 0;
+            this.NormalText.Text = "";
+            // 
+            // EncrypterText
+            // 
+            this.EncrypterText.Location = new System.Drawing.Point(12, 225);
+            this.EncrypterText.Name = "EncrypterText";
+            this.EncrypterText.Size = new System.Drawing.Size(401, 87);
+            this.EncrypterText.TabIndex = 1;
+            this.EncrypterText.Text = "";
+            // 
+            // CryptaButton
+            // 
+            this.CryptaButton.Location = new System.Drawing.Point(12, 25);
+            this.CryptaButton.Name = "CryptaButton";
+            this.CryptaButton.Size = new System.Drawing.Size(128, 47);
+            this.CryptaButton.TabIndex = 2;
+            this.CryptaButton.Text = "Crypta";
+            this.CryptaButton.UseVisualStyleBackColor = true;
+            this.CryptaButton.Click += new System.EventHandler(this.CryptaButton_Click);
+            // 
+            // DecryptaButton
+            // 
+            DecryptaButton.Location = new System.Drawing.Point(285, 25);
+            DecryptaButton.Name = "DecryptaButton";
+            DecryptaButton.Size = new System.Drawing.Size(128, 47);
+            DecryptaButton.TabIndex = 3;
+            DecryptaButton.Text = "Decrypta";
+            DecryptaButton.UseVisualStyleBackColor = true;
+            DecryptaButton.Click += new System.EventHandler(this.DecryptaButton_Click);
+            // 
+            // PassphraseText
+            // 
+            this.PassphraseText.Location = new System.Drawing.Point(110, 85);
+            this.PassphraseText.Name = "PassphraseText";
+            this.PassphraseText.Size = new System.Drawing.Size(303, 22);
+            this.PassphraseText.TabIndex = 4;
+            this.PassphraseText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Passphase";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(425, 339);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PassphraseText);
+            this.Controls.Add(DecryptaButton);
+            this.Controls.Add(this.CryptaButton);
+            this.Controls.Add(this.EncrypterText);
+            this.Controls.Add(this.NormalText);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox NormalText;
+        private System.Windows.Forms.RichTextBox EncrypterText;
+        private System.Windows.Forms.Button CryptaButton;
+        private System.Windows.Forms.TextBox PassphraseText;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ProttoEncrypter/Form1.cs
+++ ProttoEncrypter/Form1.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,147 @@ namespace ProttoEncrypter
 {
     public partial class Form1 : Form
     {
+        public class StringCipher
+        {
+            private const int Keysize = 256;
+
+            private const int DerivationIterations = 1000;
+
+            public string Encrypt(string plainText, string passPhrase)
+            {
+                var saltStringBytes = Generate256BitsOfRandomEntropy();
+                var ivStringBytes = Generate256BitsOfRandomEntropy();
+                var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+                using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+                {
+                    var keyBytes = password.GetBytes(Keysize / 8);
+                    using (var symmetricKey = new RijndaelManaged())
+                    {
+                        symmetricKey.BlockSize = 256;
+                        symmetricKey.Mode = CipherMode.CBC;
+                        symmetricKey.Padding = PaddingMode.PKCS7;
+                        using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                        {
+                            using (var memoryStream = new MemoryStream())
+                            {
+                                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                                {
+                                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                                    cryptoStream.FlushFinalBlock();
+                                    // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
+                                    var cipherTextBytes = saltStringBytes;
+                                    cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
+                                    cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
+                                    memoryStream.Close();
+                                    cryptoStream.Close();
+                                    return Convert.ToBase64String(cipherTextBytes);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            public string Decrypt(string cipherText, string passPhrase)
+            {
+                var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
+                var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
+                var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
+                var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
+
+                using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+                {
+                    var keyBytes = password.GetBytes(Keysize / 8);
+                    using (var symmetricKey = new RijndaelManaged())
+                    {
+                        symmetricKey.BlockSize = 256;
+                        symmetricKey.Mode = CipherMode.CBC;
+                        symmetricKey.Padding = PaddingMode.PKCS7;
+                        using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                        {
+                            using (var memoryStream = new MemoryStream(cipherTextBytes))
+                            {
+                                using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                                {
+                                    var plainTextBytes = new byte[cipherTextBytes.Length];
+                                    var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                                    memoryStream.Close();
+                                    cryptoStream.Close();
+                                    return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            private byte[] Generate256BitsOfRandomEntropy()
+            {
+                var randomBytes = new byte[32];
+                using (var rngCsp = new RNGCryptoServiceProvider())
+                {
+                    rngCsp.GetBytes(randomBytes);
+                }
+                return randomBytes;
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void CryptaButton_Click(object sender, EventArgs e)
+        {
+            var testoCifrato = CryptaTesto(NormalText.Text, PassphraseText.Text);
+            NormalText.Text = "";
+            EncrypterText.Text = testoCifrato;
+        }
+
+        public string CryptaTesto(string contenuto, string passphase)
+        {
+            string testoCryptato = "";
+            StringCipher cifratore = new StringCipher();
+            try
+            {
+                if (string.IsNullOrEmpty(contenuto)) throw new Exception("Testo da cryptare vuoto!");
+                if (string.IsNullOrEmpty(passphase)) throw new Exception("Passphase vuota!");
+                testoCryptato = cifratore.Encrypt(contenuto, passphase);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Errore Cryptazione", MessageBoxButtons.OK);
+            }
+
+            return testoCryptato;
+        }
+        public string DecryptaTesto(string contenuto, string passphase)
+        {
+            string testoDecryptato = "";
+            StringCipher cifratore = new StringCipher();
+            try
+            {
+                if (string.IsNullOrEmpty(contenuto)) throw new Exception("Testo da decrittare vuoto!");
+                if (string.IsNullOrEmpty(passphase)) throw new Exception("Passphase vuota!");
+                testoDecryptato = cifratore.Decrypt(contenuto, passphase);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Errore decrittazione", MessageBoxButtons.OK);
+            }
+
+            return testoDecryptato;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DecryptaButton_Click(object sender, EventArgs e)
+        {
+            var testoDecrittato = DecryptaTesto(EncrypterText.Text, PassphraseText.Text);
+            EncrypterText.Text = "";
+            NormalText.Text = testoDecrittato;
         }
     }
 }
